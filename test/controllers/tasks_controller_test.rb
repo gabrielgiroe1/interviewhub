a8754ef373e3e2ec95b62b96498eb9577a9bc284
gabrielgiroe1 <gabriel.giroe@gmail.com>
2025-07-29 14:33:45 +0300

    Add foreign key to task and fix tests

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,33 +1,53 @@
 require "test_helper"
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in_as(@user)
+    @task = tasks(:one)
+  end
+
+  private
+
+  def sign_in_as(user)
+    post session_url, params: { email_address: user.email_address, password: "password" }
+  end
   test "should get index" do
-    get tasks_index_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get tasks_create_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get tasks_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get tasks_update_url
-    assert_response :success
-  end
-
-  test "should get delete" do
-    get tasks_delete_url
+    get tasks_url
     assert_response :success
   end
 
   test "should get new" do
-    get tasks_new_url
+    get new_task_url
     assert_response :success
+  end
+
+  test "should create task" do
+    assert_difference("Task.count") do
+      post tasks_url, params: { task: { title: "Test task", status: "pending", due_date: Time.current + 1.day } }
+    end
+    assert_redirected_to tasks_url
+  end
+
+  test "should show task" do
+    get task_url(@task)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_task_url(@task)
+    assert_response :success
+  end
+
+  test "should update task" do
+    patch task_url(@task), params: { task: { title: "Updated title" } }
+    assert_redirected_to tasks_url
+  end
+
+  test "should destroy task" do
+    assert_difference("Task.count", -1) do
+      delete task_url(@task)
+    end
+    assert_redirected_to tasks_url
   end
 end
